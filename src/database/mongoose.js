7d2449444a1fcc3965ec16e7dae919d6f2704b81
mehdi <hamar3106@gmail.com>
2023-06-17 03:53:02 +0100

    refactor connection to db code

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
 
-if (process.env.MONGODB_URL) mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log('Connected to the database.');
-}).catch((error) => {
-    console.log('Database connection error:', error);
-});
+const connectToDatabase = async () => {
+    try {
+        if (process.env.MONGODB_URL) {
+            // connect to db
+            await mongoose.connect(process.env.MONGODB_URL);
+            console.log("Connected to the database");
+            return mongoose.connection; // Return the connection object
+        } else {
+            throw new Error("MONGODB_URL not found in environment variables");
+        }
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        throw error;
+    }
+};
+
+
+module.exports = connectToDatabase;
