7c05311ad614eadc46d2038e06a5a933474f162a
mehdi <hamar3106@gmail.com>
2023-06-17 03:47:48 +0100

    dependency injection

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-const mongoose = require('mongoose')
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml'); // Path to your Swagger specification file
@@ -11,61 +10,54 @@ const swaggerDocument = YAML.load('./swagger.yaml'); // Path to your Swagger spe
 dotenv.config();
 
 
-// setting port
-const port =
-  process.env.NODE_ENV === "production"
-    ? process.env.APP_PORT_PROD || 3000
-    : process.env.APP_PORT_DEV || 3500;
+function appFunction(database) {
 
-// connect database
-require("./src/database/mongoose");
+  // parse request
+  app.use(express.json());
 
-// parse request
-app.use(express.json());
+  // enable cors
+  app.use(cors());
 
-// enable cors
-app.use(cors());
-
-// check health server
-app.get('/health', (req, res) => {
-  res.status(200).json({
-    status: 'Server is healthy.',
-  });
-});
-
-// check health database
-app.get('/health-database', async (req, res) => {
-  try {
-    // Execute a simple query to check the database health
-    if (!mongoose.connection.db) throw new Error('Database connection is unhealthy.')
-
-    const db = await mongoose.connection.db.admin().ping();
-
-    if (db.ok)
-      res.status(200).json({
-        success: true,
-        message: 'Database connection is healthy.',
-      });
-    else {
-      throw new Error('Database connection is unhealthy.')
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      message: error.message,
+  // check health server
+  app.get('/health', (req, res) => {
+    res.status(200).json({
+      status: 'Server is healthy.',
     });
-  }
-});
+  });
 
-// configure routers
-const labsRouters = require("./src/routers/labs.routers");
+  // check health database
+  app.get('/health-database', async (req, res) => {
+    try {
+      // Execute a simple query to check the database health
+      if (!database.db) throw new Error('Database connection is unhealthy.')
 
-app.use("/api/labs", labsRouters);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+      const db = await database.db.admin().ping();
+
+      if (db.ok)
+        res.status(200).json({
+          success: true,
+          message: 'Database connection is healthy.',
+        });
+      else {
+        throw new Error('Database connection is unhealthy.')
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  });
+
+  // configure routers
+  const labsRouters = require("./src/routers/labs.routers");
+
+  app.use("/api/labs", labsRouters);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
-// listen app on a port
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  return app
+}
+
+module.exports = appFunction;
