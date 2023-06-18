def6f77822778cc25219bcadbf59ebf6ebc9baaf
Mehdi <mehdi.yanat3106@outlook.fr>
2023-06-18 03:02:10 +0200

    clean up code by using middleware routers

--- src/routers/labs.routers.js
+++ src/routers/labs.routers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getLabs, addLab, getOneLab, updateLab, deleteLab } = require("../controllers/labs.controllers");
-const { body, param } = require("express-validator");
+const {  param } = require("express-validator");
+const validationMiddleWare = require("../middleware/validationMiddleware");
 
 const router = express.Router();
 
@@ -11,20 +12,13 @@ router.get("/", getLabs);
 router.get('/:id', param('id').isMongoId().withMessage('Invalid ID'), getOneLab)
 
 // add lab
-router.post('/', body('name').isString().withMessage('Please put a valid name').isLength({ min: 3 }).withMessage('Name length must be at least greater than 3'),
-    body('technology').isString().withMessage('Please put a valid technology').isLength({ min: 3 }).withMessage('Technology length must be at least greater than 3'),
-    body('start_date').isDate({ format: 'YYYY-MM-DD' }).withMessage('Start date invalid '),
-    body('end_date').isDate({ format: 'YYYY-MM-DD' }).withMessage('End date invalid '), addLab)
+router.post('/', validationMiddleWare , addLab)
 
 // update lab
-
-router.put('/:id', param('id').isMongoId().withMessage('Invalid ID'), body('name').isString().withMessage('Please put a valid name').isLength({ min: 3 }).withMessage('Name length must be at least greater than 3'),
-    body('technology').isString().withMessage('Please put a valid technology').isLength({ min: 3 }).withMessage('Technology length must be at least greater than 3'),
-    body('start_date').isDate({ format: 'YYYY-MM-DD' }).withMessage('Start date invalid '),
-    body('end_date').isDate({ format: 'YYYY-MM-DD' }).withMessage('End date invalid '), updateLab)
+router.put('/:id', param('id').isMongoId().withMessage('Invalid ID') , validationMiddleWare, updateLab)
 
 // delete lab
-
 router.delete('/:id', param('id').isMongoId().withMessage('Invalid ID'), deleteLab)
+
 
 module.exports = router;
